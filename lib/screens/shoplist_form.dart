2fb77532a1f433462b510d6369aaeaf756a48bd8
saras355 <saraswati.widhisari355@gmail.com>
2023-11-08 15:20:05 +0700

    menyelesaikan tutorial 7

--- lib/screens/shoplist_form.dart
+++ lib/screens/shoplist_form.dart
@@ -144,8 +144,9 @@ class _ShopFormPageState extends State<ShopFormPage> {
                                 );
                               },
                             );
+                            _formKey.currentState!.reset();
                           }
-                          _formKey.currentState!.reset();
+                          
                         },
                         child: const Text(
                           "Save",
